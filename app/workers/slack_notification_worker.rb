0b5c259f18beae53f25a14169ff2c3a76ac7b871
KeisukeMonma <keisuke.monma@techouse.jp>
2024-05-25 20:22:20 +0900

    sidekiqで時間指定できるようにした

--- app/workers/slack_notification_worker.rb
+++ app/workers/slack_notification_worker.rb
@@ -3,6 +3,8 @@
 class SlackNotificationWorker < ApplicationWorker
   # sidekiq_options queue: 'slack_notification'
 
+  # 時間指定の場合は .perform_at(時間, 引数)
+  # 非同期で即実行したい時は .perform_async(引数)
   def perform(message)
     SlackNotifier.new.send(message, 'ランチ速報')
   end
