5177bd20e364d92a6051a80868850b3960d55c22
KeisukeMonma <keisuke.monma@techouse.jp>
2024-05-25 13:35:16 +0900

    migrate

--- db/migrate/20240525035324_create_users.rb
+++ db/migrate/20240525035324_create_users.rb
@@ -3,11 +3,11 @@ class CreateUsers < ActiveRecord::Migration[7.1]
     create_table :users do |t|
       t.string :name, null: false
       t.string :email, null: false
-      t.smallint :sex, null: false, default: 0
+      t.integer :sex, null: false, default: 0
       t.text :description
 
-      t.references :role_id, foreign_key: true
-      t.references :employement_type_id, null: false, foreign_key: true
+      t.references :role, foreign_key: true
+      t.references :employment_type, null: false, foreign_key: true
 
       t.timestamps
     end
